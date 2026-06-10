f939c9dd36e5dbc8c19c63813793bc9e45c3538e
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-06-10 18:54:47 -0300

    feat: create tenants API with Prisma

--- backend/src/tenants/tenants.controller.ts
+++ backend/src/tenants/tenants.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TenantsService } from './tenants.service';
+import { CreateTenantDto } from './dto/create-tenant.dto';
 
 @Controller('tenants')
-export class TenantsController {}
+export class TenantsController {
+  constructor(private readonly tenantsService: TenantsService) {}
+
+  @Post()
+  create(@Body() createTenantDto: CreateTenantDto) {
+    return this.tenantsService.create(createTenantDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.tenantsService.findAll();
+  }
+}
--- backend/src/tenants/tenants.service.ts
+++ backend/src/tenants/tenants.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateTenantDto } from './dto/create-tenant.dto';
 
 @Injectable()
-export class TenantsService {}
+export class TenantsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateTenantDto) {
+    return this.prisma.tenant.create({
+      data,
+    });
+  }
+
+  async findAll() {
+    return this.prisma.tenant.findMany();
+  }
+}
