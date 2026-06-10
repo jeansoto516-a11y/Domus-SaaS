fbfce421ff280c69d0acafa5e39fec1c74541921
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-06-10 18:36:33 -0300

    feat: configure Prisma integration with NestJS

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { TenantsModule } from './tenants/tenants.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TenantsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/prisma/prisma.service.spec.ts
+++ backend/src/prisma/prisma.service.spec.ts
@@ -1,18 +1,9 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { PrismaService } from './prisma.service';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
-describe('PrismaService', () => {
-  let service: PrismaService;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PrismaService],
-    }).compile();
-
-    service = module.get<PrismaService>(PrismaService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
