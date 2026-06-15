919f5ad26c990656806eaf75a3cfd9854d26cb27
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-06-15 19:12:52 -0300

    feat: setup tenants module and prisma integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TenantsModule } from './tenants/tenants.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, TenantsModule],
+  imports: [PrismaModule, TenantsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
