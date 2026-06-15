07c717ff24a9be84c350a1441afc942db8baef10
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-06-15 19:34:28 -0300

    feat: setup users and auth modules

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService) {}
+
+    async create(data: {
+    name: string;
+    email: string;
+    password: string;
+    tenantId: string;
+    }) {
+    const passwordHash = await bcrypt.hash(data.password, 10);
+
+    return this.prisma.user.create({
+        data: {
+        name: data.name,
+        email: data.email,
+        passwordHash,
+        tenantId: data.tenantId,
+        },
+    });
+    }
+
+    async findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+        where: {
+        email,
+        },
+    });
+    }
+}
